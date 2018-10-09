1b00d9b2f4cb7af2d5b4b5583f1c143f0c802bd9
Алексей Хачатуров <lug4rd@protonmail.com>
2018-10-09 23:46:14 +0300

    Mobile input

--- Assets/Scripts/HeroController/HeroController.cs
+++ Assets/Scripts/HeroController/HeroController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using Zenject;
+using UnityStandardAssets.CrossPlatformInput;
 
 namespace OneEv9gPr0ject
 {
@@ -10,28 +11,28 @@ namespace OneEv9gPr0ject
     {
         private readonly HeroControllerState _state;
 
-        //
-        HeroFacade _hero;
-
-        public HeroController(HeroControllerState state, HeroFacade hero) {
+        public HeroController(HeroControllerState state) {
             _state = state;
-            //
-            _hero = hero;
         }
 
         public void Tick() {
-            // FIXME: should use virtual buttons
-            // for fast reassign
-            _state.IsMovingLeft = Input.GetKey(KeyCode.A);
-            _state.IsMovingRight = Input.GetKey(KeyCode.D);
-            _state.IsFiring = Input.GetKey(KeyCode.Space);
-            _state.IsPunching = Input.GetKey(KeyCode.J);
-            _state.IsKicking = Input.GetKey(KeyCode.K);
+            //// FIXME: should use virtual buttons
+            //// for fast reassign
+            //_state.IsMovingLeft = Input.GetKey(KeyCode.A);
+            //_state.IsMovingRight = Input.GetKey(KeyCode.D);
+            //_state.IsFiring = Input.GetKey(KeyCode.Space);
+            //_state.IsPunching = Input.GetKey(KeyCode.J);
+            //_state.IsKicking = Input.GetKey(KeyCode.K);
+            //_state.DeviationX = (_state.IsMovingLeft ? -1 : (_state.IsMovingRight ? 1 : 0));
 
-            _state.DeviationX = (_state.IsMovingLeft ? -1 : (_state.IsMovingRight ? 1 : 0));
-
-            //
-            if (Input.GetKey(KeyCode.O)) { _hero.Die();  }
+            _state.DeviationX = CrossPlatformInputManager.GetAxis("Horizontal");
+            _state.DeviationX = (_state.DeviationX < -0.1f ? -1 : _state.DeviationX > 0.1f ? 1 : 0);
+            _state.IsMovingLeft = (_state.DeviationX < -0.1f);
+            _state.IsMovingRight = (_state.DeviationX > 0.1f);
+            _state.IsFiring = false;
+            _state.IsPunching = CrossPlatformInputManager.GetButton("Punch");
+            _state.IsKicking = CrossPlatformInputManager.GetButton("Kick");
+           
         }
     }
 }
